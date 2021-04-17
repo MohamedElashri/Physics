5ac53a2a1cee0746332dd02386a2590bd7f53284
Mohamed <s-mohamed.elashri@zewailcity.edu.eg>
2021-04-17 06:26:09 -0400

    more optimization
    adding progress bar tracking through rich and make a choice between it and tqdm

--- Statistical Mechanics/Ising/Python/2d/IsingModel.py
+++ Statistical Mechanics/Ising/Python/2d/IsingModel.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+ #coding: utf-8
 #----------------------------------------------------------------------
 ## 2d Ising model Monte-Carlo Simulation
 ## Author: Mohamed Elashri 
@@ -49,19 +49,7 @@ is fundementally same as i-th lattice point denotation above.s
 * * * * * * * * 1
 1 2 3 4 5 6 7 8 
 
-
-
 '''
-#----------------------------------------------------------------------
-##  Import needed python libraries 
-#----------------------------------------------------------------------
-import matplotlib.pyplot as plt
-from numba import jit # wonderful optimization compiler. 
-import numpy as np  # we can't work in python without that in physics (maybe we can if we are not lazy enough)
-import random # we are doing MC simulation after all!!
-import time # for time estimation 
-from tqdm import tqdm # fancy progress bars for loops
-
 
 
 '''
@@ -74,18 +62,33 @@ also I made the code avilable on colab and can be accssed here (without much com
 '''
 
 
+#----------------------------------------------------------------------
+##  Import needed python libraries 
+#----------------------------------------------------------------------
+import matplotlib.pyplot as plt
+from numba import jit # wonderful optimization compiler. 
+import numpy as np  # we can't work in python without that in physics (maybe we can if we are not lazy enough)
+import random # we are doing MC simulation after all!!
+import time # for time estimation 
+from tqdm import tqdm # fancy progress bars for loops , use this line if working with .py script
+#from tqdm.notebook import tqdm # use this if working with jupyter notebook to avoid printing a new line for each iteration
+from rich.progress import track # we can use amazin rich library to do the same (more interesting output) change "tqdm" later to "track"
+
 # Define parameters 
+
 B = 0; # Magnetic field strength
 L = 50; # Lattice size (width)
 s = np.random.choice([1,-1],size=(L,L)) # Begin with random spin sites with values (+1 or -1) for up or down spins. 
 n= 1000 * L**2 # number of MC sweeps 
-T = np.arange(1.6,3.25,0.01) # Initlaize temperature range (the range includes critical temperature) > takes form np.arange(start,stop,step)
+Temperature = np.arange(1.6,3.25,0.01) # Initlaize temperature range (the range includes critical temperature) > takes form np.arange(start,stop,step)
+  
+
 
 '''
 Energy of the lattice calculations. 
 The energy here is simply the sum of interactions between spins divided by the total number of spins
 '''
-@jit(nopython=True) # wonderful jit optimization compiler in its high performance mode.
+@jit(nopython=True) # wonderful jit optimization compiler in its high performance mode
 def calcE(s):
     E = 0
     for i in range(L):
@@ -94,58 +97,57 @@ def calcE(s):
     return E/L**2
 
 '''
-Magnetization calculation: Calculate the Magnetization of a given configuration
+Calculate the Magnetization of a given configuration
 Magnetization is the sum of all spins divided by the total number of spins
 
 '''
-@jit(nopython=True) # wonderful jit optimization compiler in its high performance mode.
+@jit(nopython=True) # wonderful jit optimization compiler in its high performance mode
 def calcM(s):
     m = np.abs(s.sum())
     return m/L**2
 
-'''
-Calculate interaction energy between spins. Assume periodic boundaries
-Interaction energy will be the difference in energy due to flipping spin i,j 
-'''
+# Calculate interaction energy between spins. Assume periodic boundaries
+# Interaction energy will be the difference in energy due to flipping spin i,j 
+# (Example: 2*spin_value*neighboring_spins)
 @jit(nopython=True) # wonderful jit optimization compiler in its high performance mode
-def dE(s,i,j):
+def dE(s,i,j): # change in energy function
     #top
     if i == 0:
-        t = s[L-1,j]  # periodic boundary
+        t = s[L-1,j]  # periodic boundary (top)
     else:
         t = s[i-1,j]
     #bottom
     if i == L-1:
-        r = s[i,0]  # periodic boundary
+        b = s[0,j]  # periodic boundary (bottom)
+    else:
+        b = s[i+1,j]
+    #left
+    if j == 0:
+        l = s[i,L-1]  # periodic boundary (left)
+    else:
+        l = s[i,j-1]
+    #right
+    if j == L-1:
+        r = s[i,0]  # periodic boundary  (right)
     else:
         r = s[i,j+1]
     return 2*s[i,j]*(t+b+r+l)  # difference in energy is i,j is flipped
 
-
-'''
-Monte-carlo sweep implementation
-'''
- 
+# Monte-carlo sweep implementation
 @jit(nopython=True) # wonderful jit optimization compiler in its high performance mode
-def mc(s,Temp,n):
+def mc(s,Temp,n):   
     for m in range(n):
         i = random.randrange(L)  # choose random row
         j = random.randrange(L)  # choose random column
         ediff = dE(s,i,j)
-        if ediff <= 0:        # if the change in energy is negative
+        if ediff <= 0: # if the change in energy is negative
             s[i,j] = -s[i,j]  # accept move and flip spin
-        elif random.random() < np.exp(-ediff/Temp): # if not accept it with probability exp^{-dE/kT}
+        elif random.random() < np.exp(-ediff/Temp): # if not accept it with probability exp^{-dU/kT}
             s[i,j] = -s[i,j]
     return s
 
-
-'''
-Calculate the physical quantities of interest 
-1- Energy 
-2- Mangtization 
-3- Heat Capacity
-'''
-@jit(nopython=True)
+# Compute physical quantities
+@jit(nopython=True)  # wonderful jit optimization compiler in its high performance mode
 def physics(s,T,n):
     En = 0
     En_sq = 0
@@ -165,17 +167,25 @@ def physics(s,T,n):
     return En_avg, mag, CV
 
 # Inititalize magnetization, average energy and heat capacity
-mag = np.zeros(len(T))
-En_avg = np.zeros(len(T))
-CV = np.zeros(len(T))
+mag = np.zeros(len(Temperature))
+En_avg = np.zeros(len(Temperature))
+CV = np.zeros(len(Temperature))
 
+start = time.time()
 
 # Simulate at particular temperatures (T) and compute physical quantities (Energy, heat capacity and magnetization)
-for ind, T in enumerate(tqdm(T)):
-    # sweep spins
+for ind, T in enumerate(track(Temperature)):
+    # Sweeps spins
     s = mc(s,T,n)
     # Compute physical quanitites with 1000 sweeps per spin at temperature T
     En_avg[ind], mag[ind], CV[ind] = physics(s,T,n)
+end = time.time()
+print("Time it took in seconds is = %s" % (end - start))
+
+time = (end - start)/60
+print('It took ' + str(time) + ' minutes to execute the code')
+
+# It took about 30 minutes on my Mac meachine (not bad)
 
 
 
@@ -185,20 +195,18 @@ for ind, T in enumerate(tqdm(T)):
 
 
 f = plt.figure(figsize=(18, 10)); # plot the calculated values  
-
-
 sp =  f.add_subplot(2, 2, 1 );
-plt.plot(T, En_avg, marker='.', color='IndianRed')
+plt.plot(Temperature, En_avg, marker='.', color='IndianRed')
 plt.xlabel("Temperature (T)", fontsize=20);
 plt.ylabel("Energy ", fontsize=20);         plt.axis('tight');
 
 sp =  f.add_subplot(2, 2, 2 );
-plt.plot(T, abs(mag), marker='.', color='RoyalBlue')
+plt.plot(Temperature, abs(mag), marker='.', color='RoyalBlue')
 plt.xlabel("Temperature (T)", fontsize=20); 
 plt.ylabel("Magnetization ", fontsize=20);   plt.axis('tight');
 
 sp =  f.add_subplot(2, 2, 3 );
-plt.plot(T, CV, marker='.', color='IndianRed')
+plt.plot(Temperature, CV, marker='.', color='IndianRed')
 plt.xlabel("Temperature (T)", fontsize=20);  
 plt.ylabel("Specific Heat ", fontsize=20);   plt.axis('tight');   
 
@@ -207,8 +215,5 @@ plt.suptitle("Simulation of 2D Ising Model by Metropolis Algorithm\n" + "Lattice
     L) + "\n" + "External Magnetic Field(B)=" + str(B) + "\n" + "Metropolis Step=" + str(n))
 
 
+
 plt.show() # function to show the plots
-
-
-
-
