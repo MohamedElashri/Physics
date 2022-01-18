8e0fa98fd4f22cc145729a2179fbcc50689a02f6
Mohamed Elashri <s-mohamed.elashri@zewailcity.edu.eg>
2022-01-18 13:26:02 -0500

    Update solar_system_mini.py

--- Astrophysics/Solar System/solar_system_mini.py
+++ Astrophysics/Solar System/solar_system_mini.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from astropy.time import Time
 from astroquery.jplhorizons import Horizons
 import warnings
 
+matplotlib.use('TkAgg')
 warnings.filterwarnings("ignore",category=DeprecationWarning) # supress deprecation warnings 
 
 
 sim_start_date = "2022-01-01"     # simulating a solar system starting starting from this date
-sim_duration = 10 * 365                # simulation duration in days (must be integer)
+sim_duration = 2 * 365            # simulation duration in days (must be integer)
 m_earth = 5.9722e24 / 1.98847e30  # relative mass of earth with respect to the sun
-m_moon = 7.3477e22 / 1.98847e30
+m_moon = 7.3477e22 / 1.98847e30   # relative mass of moon with respect to the earth
 
-class Object:                   # define the objects: the Sun, Earth, Mercury, etc
+class Object:                   # define the objects: the Sun, Earth, etc
     def __init__(self, name, rad, color, r, v):
         self.name = name
         self.r    = np.array(r, dtype=float)
@@ -52,21 +54,20 @@ class SolarSystem:
         return plots + lines + [self.timestamp]
 
 plt.style.use('dark_background')
-fig = plt.figure(figsize=[6, 6])
+fig = plt.figure(figsize=[10, 10])
 ax = plt.axes([0., 0., 1., 1.], xlim=(-1.8, 1.8), ylim=(-1.8, 1.8))
 ax.set_aspect('equal')
 ax.axis('off')
-ss = SolarSystem(Object("Sun", 28, 'blue', [0, 0, 0], [0, 0, 0]))
+ss = SolarSystem(Object("Sun", 28, 'red', [0, 0, 0], [0, 0, 0]))
 ss.time = Time(sim_start_date)
 ss.time.format='jd'
 
-
-colors = ['gray', 'orange', 'red', 'chocolate']
-sizes = [0.38, 0.95, 1., 0.53]
-names = ['Mercury', 'Venus', 'Earth', 'Mars']
-texty = [.47, .73, 1, 1.5]
-for i, nasaid in enumerate([1, 2, 3, 4]):  # The 1st, 2nd, 3rd, 4th planet in solar system
-    obj = Horizons(id=nasaid, location="@sun", epochs=ss.time, id_type='id').vectors()
+colors = ['gray', 'orange', 'blue', 'chocolate', 'green']
+sizes = [0.38, 0.95, 1., 0.53, 0.78]
+names = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter']
+texty = [0.47, 0.82, 1., 1.5, 3.2]
+for i, nasaid in enumerate([1, 2, 3, 4, 5]):  # The 1st, 2nd, 3rd, 4th, 5th planet in solar system
+    obj = Horizons(id=nasaid, location="@sun", epochs=ss.time).vectors()
     ss.add_planet(Object(nasaid, 20 * sizes[i], colors[i], 
                          [np.double(obj[xi]) for xi in ['x', 'y', 'z']], 
                          [np.double(obj[vxi]) for vxi in ['vx', 'vy', 'vz']]))
